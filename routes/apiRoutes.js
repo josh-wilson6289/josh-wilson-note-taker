5c4d9dcc1c46079060d8fdad479fe527e1484a97
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-07 16:15:45 -0500

    got post requests working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,11 @@ module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     res.json(db);
   });
+
+  app.post("/api/notes", function(req, res) {
+    db.push(req.body);
+    res.json(db);
+  })
 }
 
 
