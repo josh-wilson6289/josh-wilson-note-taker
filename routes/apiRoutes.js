c08f6595fc38d93234db72bf1a8111529148e64f
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-07 15:22:26 -0500

    moving away from post issues in API and just trying to get the data to show on the page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,18 @@
 const db = require("../db/db.json");
+const fs = require("fs");
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     res.json(db);
   });
-
-app.post("/api/notes", function(req, res) {
-  let newNote = req.body
-  db.push(newNote);
-  console.log("new note");
-  });
 }
+
+// app.post("/api/notes", function(req, res) {
+//   let newNote = req.body;
+
+//   fs.appendFile("db", newNote, function(err) {
+//     if (err) throw err;
+//     console.log("saved");
+//   });
+// });
+// }
