cc28cc4ae02562bea91cdc68fd14055fc656d039
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-06 23:04:31 -0500

    added server.js, apiRoutes.js, and htmlRoutes.js files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`);
+});
