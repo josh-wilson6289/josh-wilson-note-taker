e9962b30d68cc5fba5df5d081d2fca4344b17293
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-06 23:38:43 -0500

    got require for apiRoutes in server.js working

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const db = require("../db/db.json");
+
+module.exports = function(app) {
+  app.get("/api/notes", function(req, res) {
+    res.json(db);
+  });
+
+app.post("/api/notes", function(req, res) {
+  db.push(req.body)
+  res.json(true);
+});
+}
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ let PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("../routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, function() {
   console.log(`App listening on port ${PORT}`);
