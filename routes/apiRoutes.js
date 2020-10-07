5cec7d235b4a6e394c3ca6193adf734c92ee7bea
Josh Wilson <joshua.wilson6289@gmail.com>
2020-10-07 18:28:30 -0500

    added an ID to each new note post request.  need to use that in order to delete.  still need to add delete request functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const db = require("../db/db.json");
 const fs = require("fs");
+const uniqid = require("uniqid");
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
@@ -7,9 +8,19 @@ module.exports = function(app) {
   });
 
   app.post("/api/notes", function(req, res) {
-    db.push(req.body);
+    let newNote = req.body;
+    let newId = uniqid();
+    newNote.id = newId;
+    console.log(newNote);
+    db.push(newNote);
     res.json(db);
-  })
+  });
+
+  app.delete("/api/notes:id", function(req, res) {
+    db.splice();
+    res.json(db);
+  });
+
 }
 
 
